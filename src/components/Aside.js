44f09d551b213d6ac8f8b5a602e80a6aaff6f52b
g <umacgamze49@gmail.com>
2021-05-19 13:56:21 +0200

    Aufgabe

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -5,7 +5,7 @@ function Aside() {
         <div className="row">
             <div className="col-12 col-md-3">
             <h4>Aside 1</h4>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
+            <p>Lorem ipsum dolor sit aaamet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et</p>
             </div>
             <div className="col-12 col-md-6">
             <h4>Article</h4>
